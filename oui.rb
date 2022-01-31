0446351802eca1667831df1c79bd0c53e0446b3b
Matt Love <matt@stunninglyclear.com>
2022-01-31 10:40:58 -0700

    Brew formula update for oui version v0.1.8

--- oui.rb
+++ oui.rb
@@ -5,21 +5,21 @@
 class Oui < Formula
   desc "MAC Address CLI Toolkit"
   homepage "https://github.com/thatmattlove/oui"
-  version "0.1.7"
+  version "0.1.8"
   license "BSD-3-Clause-Clear"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/thatmattlove/oui/releases/download/v0.1.7/oui_0.1.7_macOS_amd64.tar.gz"
-      sha256 "5e71bba12a9f140edc0d9c9dfaf9e326fb9e87c19f6947f00bccc96e6cfa1fa5"
+    if Hardware::CPU.arm?
+      url "https://github.com/thatmattlove/oui/releases/download/v0.1.8/oui_0.1.8_darwin_arm64.tar.gz"
+      sha256 "45cf5925aa6d51d5edf8a1915f3d58b841fa97de3b0ed18273326080c959dc59"
 
       def install
         bin.install "oui"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/thatmattlove/oui/releases/download/v0.1.7/oui_0.1.7_macOS_arm64.tar.gz"
-      sha256 "fb903d93eda635e391dbb3bb5bc6375e96eecf0a9ee898d6aa1845ee788eceb2"
+    if Hardware::CPU.intel?
+      url "https://github.com/thatmattlove/oui/releases/download/v0.1.8/oui_0.1.8_darwin_amd64.tar.gz"
+      sha256 "cd26e80c45569ce5aced00892f7226b84dbe25b4a372c74fcd45df5427264fba"
 
       def install
         bin.install "oui"
@@ -29,24 +29,24 @@ class Oui < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/thatmattlove/oui/releases/download/v0.1.7/oui_0.1.7_linux_armv6.tar.gz"
-      sha256 "597e74d672cfafcd9434b34774a9aa02d62c7613674b46478e5911a2818f9611"
+      url "https://github.com/thatmattlove/oui/releases/download/v0.1.8/oui_0.1.8_linux_armv6.tar.gz"
+      sha256 "997a115d0f931ca83973fcd39d69a71f1c2405c4a854fe03c392892af00f0a68"
 
       def install
         bin.install "oui"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/thatmattlove/oui/releases/download/v0.1.7/oui_0.1.7_linux_arm64.tar.gz"
-      sha256 "eab8a3c49de8d0a2f4f11310078a75b86873b9236f4aa4e4a6d1df3a7c16eb29"
+      url "https://github.com/thatmattlove/oui/releases/download/v0.1.8/oui_0.1.8_linux_arm64.tar.gz"
+      sha256 "b84ba9ec81acb2ae0ba0c79fa2405dbc83a600a8c1bccf4894b3840c878b252c"
 
       def install
         bin.install "oui"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/thatmattlove/oui/releases/download/v0.1.7/oui_0.1.7_linux_amd64.tar.gz"
-      sha256 "05a0db56917e0d52d4af37cc5dafff47359c889d2ac11c34e795f45c813f2497"
+      url "https://github.com/thatmattlove/oui/releases/download/v0.1.8/oui_0.1.8_linux_amd64.tar.gz"
+      sha256 "21df3656389bee87c4460ac6f57bead0ad489948325628d4822382f534bb5fc0"
 
       def install
         bin.install "oui"
